fd713e60e72f692e9b391160788a194a25a36d3e
Bobby <62272380+bobbyunknown@users.noreply.github.com>
2024-10-16 03:44:19 +0700

    Update installer

--- install.sh
+++ install.sh
@@ -12,39 +12,38 @@ get_latest_release_url() {
     REPO="bobbyunknown/luci-app-syscontrol"
     API_URL="https://api.github.com/repos/$REPO/releases/latest"
     DOWNLOAD_URL=$(curl -s "$API_URL" | grep "browser_download_url.*ipk" | cut -d '"' -f 4)
-    echo "$DOWNLOAD_URL"
+    
+    if [ ! -z "$DOWNLOAD_URL" ]; then
+        FILENAME=$(basename "$DOWNLOAD_URL")
+        echo "Mengunduh $FILENAME..."
+        if curl -L -o "/tmp/$FILENAME" "$DOWNLOAD_URL"; then
+            echo "/tmp/$FILENAME"
+        else
+            echo ""
+        fi
+    else
+        echo ""
+    fi
 }
 
 install_package() {
     echo "Menginstal $PACKAGE_NAME..."
     opkg update 
     
-    DOWNLOAD_URL=$(get_latest_release_url)
-    if [ -z "$DOWNLOAD_URL" ]; then
-        echo "Gagal mendapatkan URL unduhan. Silakan coba lagi nanti."
-        return 1
-    fi
-    
-    FILENAME=$(basename "$DOWNLOAD_URL")
-    echo "Mengunduh $FILENAME..."
-    if ! wget -O "/tmp/$FILENAME" "$DOWNLOAD_URL"; then
-        echo "Gagal mengunduh file. Silakan periksa koneksi internet Anda."
-        return 1
-    fi
-    
-    if [ ! -f "/tmp/$FILENAME" ]; then
-        echo "File tidak ditemukan setelah unduhan. Silakan coba lagi."
+    DOWNLOADED_FILE=$(get_latest_release_url)
+    if [ -z "$DOWNLOADED_FILE" ]; then
+        echo "Gagal mendapatkan atau mengunduh file. Silakan coba lagi nanti."
         return 1
     fi
     
     echo "Menginstal paket..."
-    if ! opkg install "/tmp/$FILENAME"; then
+    if ! opkg install "$DOWNLOADED_FILE"; then
         echo "Gagal menginstal paket. Silakan coba opsi force install."
-        rm -f "/tmp/$FILENAME"
+        rm -f "$DOWNLOADED_FILE"
         return 1
     fi
     
-    rm -f "/tmp/$FILENAME"
+    rm -f "$DOWNLOADED_FILE"
     echo "Instalasi $PACKAGE_NAME selesai."
     read -p "Tekan Enter untuk melanjutkan..."
 }
